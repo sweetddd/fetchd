b30665b865f28b4445d9cd049b5ccc7451b06a03
henry <henry@everylink.ai>
2022-07-26 16:07:37 +0800

    修改部署文件

--- testnet_init.sh
+++ testnet_init.sh
@@ -4,9 +4,9 @@ rm -rf data/node0
 rm -rf data/node1
 rm -rf data/node2
 
-fetchd init node0 --chain-id localnet_9002-1 --home data/node0
-fetchd init node1 --chain-id localnet_9002-1 --home data/node1
-fetchd init node2 --chain-id localnet_9002-1 --home data/node2
+fetchd init node0 --chain-id testnet_9002-1 --home data/node0
+fetchd init node1 --chain-id testnet_9002-1 --home data/node1
+fetchd init node2 --chain-id testnet_9002-1 --home data/node2
 
 
 #fetchd keys add node1
@@ -23,6 +23,8 @@ cat data/node0/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["mi
 cat data/node0/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="afet"' > data/node0/config/tmp_genesis.json && mv data/node0/config/tmp_genesis.json data/node0/config/genesis.json
 
 
+fetchd add-genesis-account fetch18ehzjqle9d8a7qgtjyx8vllv9skwhflul2lmza 10000000000000000000000000000afet --home data/node0
+fetchd add-genesis-account fetch1kqn6f3kmu70smsx469fwtsvaqq3t0qerqgjlkt 10000000000000000000000000000afet --home data/node0
 fetchd add-genesis-account fetch1320mysnjer5r8vesmf33tlx26syscvek5824h7 100000000000000000000000000afet --home data/node0
 fetchd add-genesis-account fetch1n7yrkyhaq6f8znp09zlxcsxn4ldepqwn9fh7fq 100000000000000000000000000afet --home data/node0
 
@@ -33,9 +35,9 @@ fetchd add-genesis-account node2 100000000000000000000afet --home data/node1
 fetchd add-genesis-account node3 100000000000000000000afet --home data/node2
 
 
-fetchd gentx  node1 100000000000000000000afet --home data/node0  --node-id $(fetchd tendermint show-node-id --home data/node0) --chain-id localnet_9002-1
-fetchd gentx  node2  100000000000000000000afet --home data/node1  --node-id $(fetchd tendermint show-node-id --home data/node1) --chain-id localnet_9002-1
-fetchd gentx  node3 100000000000000000000afet --home data/node2  --node-id $(fetchd tendermint show-node-id --home data/node2) --chain-id localnet_9002-1
+fetchd gentx  node1 100000000000000000000afet --home data/node0  --node-id $(fetchd tendermint show-node-id --home data/node0) --chain-id testnet_9002-1
+fetchd gentx  node2  100000000000000000000afet --home data/node1  --node-id $(fetchd tendermint show-node-id --home data/node1) --chain-id testnet_9002-1
+fetchd gentx  node3 100000000000000000000afet --home data/node2  --node-id $(fetchd tendermint show-node-id --home data/node2) --chain-id testnet_9002-1
 
 
 
